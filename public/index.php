7db50c9bf0365ebe41387f8765737076380cfdb8
Matteo Grado <111707247+MatteoGrado@users.noreply.github.com>
2024-02-05 16:29:05 +0100

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CDN.de</title>
+    <link rel="stylesheet" href="assets/css/start-main/style.css">
+</head>
+<body>
+    <div class="nav">
+        <h1 class="header">
+            <a href="index.php">CDN-AG.de</a>
+        </h1>
+        <div class="navbar">
+            <a href="#">Über uns</a>
+            <a href="#">Unsere Services</a>
+            <a href="#">Support</a>
+            <a href="#">Ihre Daten</a>
+            <a href="#">Standorte</a>
+            <a href="#">Kontakt</a>
+            <a href="#">Login</a>
+        </div>
+    </div>
+    <div class="main-img">
+        <img class="img" src="assets/img/jpg/main-firma-image.jpg" alt="Firmengebäude">
+    </div>
+    <div class="center-first">
+        <h1 class="head">Willkommen bei der CDN-AG</h1>
+    </div>
+    <div class="container">
+        <img src="assets/img/png/main-img01.png" class="img00" alt="Netzwerk Bild">
+        <img src="assets/img/jpg/main-firma-image01.jpg" class="img01" alt="Büro Bild">
+    </div>
+        <h1 class="text-header">CDN-AG:</h1>
+    <div class="text">
+        Wir, die CDN-AG sind der Deutsche hersteller für Content delivery services Applications<br>
+        Wir sind für Sie im Support 24/7 erreichbar und versuchen Ihre Anliegen zu lösen!<br>
+        CDN-AG der zuverlässige Partner wenn es um Kontent geht.<br>
+        Egal ob herstellung oder veröffentlichung wir schaffen alles!<br>
+        Durch unsere 500 Weltweit agierenden Mitarbeiter sind wir selbstverständlich international vertreten.<br>
+        Sind Sie nicht in Deutschland? Kein Problem wir nehmen uns trozdem Zeit und treffen Sie persönlich überall auf der Welt!<br>
+    </div>
+    <div class="img-code">
+        <img src="assets/img/" class="img-code-img" alt="Bild Programmcode">
+    </div>
+</body>
+</html>
